80d26cc6b2fe094f95321f4fd878294e9420b178
José Lescano <92216756+jlesca@users.noreply.github.com>
2021-11-18 15:39:55 -0300

    Create python-conditions.py

---
+++ python-conditions.py
@@ -0,0 +1,14 @@
+# We can compare two or more variables with the logical operators.
+# For example, we want to display a message if a condition is true.
+
+print('''IF A CONDITION IS TRUE or FALSE
+a = 40
+b = 100''') 
+
+a = 40
+b = 100
+if a < b:
+    print('B is grater than A - The condition is TRUE')
+else:
+    print('B is not greater than A - The condition is FALSE')
+input()
